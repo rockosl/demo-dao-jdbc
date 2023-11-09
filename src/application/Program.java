5d8d691d13e826f586dd45c2d99051918d1884cc
ricardoNote <rockobaschirotto@gmail.com>
2023-11-08 21:39:25 -0300

    teste commit note 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ public class Program {
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		System.out.println(seller);
-		System.out.println("teste commit note");
+		System.out.println("teste commit note 2222");
 
 	}
 
