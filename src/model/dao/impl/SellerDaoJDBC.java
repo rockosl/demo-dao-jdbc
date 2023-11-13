f9f176c63c1ede065d311cb0ae7825aef2181647
Ricardo <rockobaschirotto@gmail.com>
2023-11-13 10:25:50 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -30,8 +30,7 @@ public class SellerDaoJDBC implements SellerDao {
 				INSERT INTO seller
 				(Name, Email, BirthDate, BaseSalary, DepartmentId)
 				VALUES
-				(?, ?, ?, ?, ?)
-				""";
+				(?, ?, ?, ?, ?)	""";
 		
 		PreparedStatement st = null;
 		try {
@@ -64,7 +63,29 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		String sql = """
+				UPDATE seller
+				SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?
+				WHERE Id = ? """;
+		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
